00c1e5d019c2e409c725129ce9a9cb8b84258267
Anukul Kumar <anukul.kumar@cyble.com>
2023-07-25 19:05:54 +0530

    updated Imports: odin.*

--- odin/__init__.py
+++ odin/__init__.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import
 
-from odin_client import OdinClient
-from models.certificate_search_response_model import CertificateSearchResponse
-from models.get_host_count import GetHostsCountResponse
-from models.get_hosts_ip_details_response import GetHostsIpDetailsResponse
-from models.search_hosts_response_model import SearchHostsResponse
-from models.get_hosts_summary_response import GetHostSummaryResponse
-from models.get_certificate_count import GetCertificatesCountResponse
-from models.get_certificate_hash_details import GetCertificateHashDetails
-from models.get_certificates_summary import GetCertificatesSummaryResponse
+from odin.odin_client import OdinClient
+from odin.models.certificate_search_response_model import CertificateSearchResponse
+from odin.models.get_host_count import GetHostsCountResponse
+from odin.models.get_hosts_ip_details_response import GetHostsIpDetailsResponse
+from odin.models.search_hosts_response_model import SearchHostsResponse
+from odin.models.get_hosts_summary_response import GetHostSummaryResponse
+from odin.models.get_certificate_count import GetCertificatesCountResponse
+from odin.models.get_certificate_hash_details import GetCertificateHashDetails
+from odin.models.get_certificates_summary import GetCertificatesSummaryResponse
 
 
 __all__ = [
--- odin/odin_client.py
+++ odin/odin_client.py
@@ -1,13 +1,13 @@
 import requests
-from models.certificate_search_response_model import CertificateSearchResponse
-from models.get_host_count import GetHostsCountResponse
-from models.get_hosts_ip_details_response import GetHostsIpDetailsResponse
-from models.search_hosts_response_model import SearchHostsResponse
-from models.get_hosts_summary_response import GetHostSummaryResponse
-from models.get_certificate_count import GetCertificatesCountResponse
-from models.get_certificate_hash_details import GetCertificateHashDetails
-from models.get_certificates_summary import GetCertificatesSummaryResponse
-from exceptions import APIException
+from odin.models.certificate_search_response_model import CertificateSearchResponse
+from odin.models.get_host_count import GetHostsCountResponse
+from odin.models.get_hosts_ip_details_response import GetHostsIpDetailsResponse
+from odin.models.search_hosts_response_model import SearchHostsResponse
+from odin.models.get_hosts_summary_response import GetHostSummaryResponse
+from odin.models.get_certificate_count import GetCertificatesCountResponse
+from odin.models.get_certificate_hash_details import GetCertificateHashDetails
+from odin.models.get_certificates_summary import GetCertificatesSummaryResponse
+from odin.exceptions import APIException
 import json
 
 class OdinClient:
